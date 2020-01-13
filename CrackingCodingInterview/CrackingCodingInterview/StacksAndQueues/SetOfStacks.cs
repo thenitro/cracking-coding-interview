b581b39a0c5ce2272aa8bd7e420e6e1ddf263d97
Timur Koshel <t.koshel@come2play.com>
2020-01-13 14:51:30 +0200

    Task 33 done

--- CrackingCodingInterview/CrackingCodingInterview/StacksAndQueues/SetOfStacks.cs
+++ CrackingCodingInterview/CrackingCodingInterview/StacksAndQueues/SetOfStacks.cs
@@ -1,27 +1,83 @@
+using System;
 using System.Collections.Generic;
 
 namespace CrackingCodingInterview.StacksAndQueues
 {
     public class SetOfStacks<T>
     {
+        private int _currentStackIndex;
         private int _stackLimit;
+        
         private List<Stack<T>> _listOfStacks;
         
         public SetOfStacks(int stackLimit)
         {
+            if (stackLimit < 1)
+            {
+                throw new ArgumentException("Stack limit must be more than zero");
+            }
+
+            _currentStackIndex = 0;
+            _listOfStacks = new List<Stack<T>>();
+            
+            _stackLimit = stackLimit;
         }
+
+        public int CurrentStackIndex => _currentStackIndex;
 
         public void Push(T item)
         {
+            if (_listOfStacks.Count == 0)
+            {
+                _listOfStacks.Add(new Stack<T>());
+            }
             
+            var stack = _listOfStacks[_currentStackIndex];
+                stack.Push(item);
+
+            if (stack.Count >= _stackLimit)
+            {
+                _currentStackIndex++;
+                _listOfStacks.Add(new Stack<T>());
+            }
         }
 
         public T Pop()
         {
+            var stack = _listOfStacks[_currentStackIndex];
+            if (stack.Count == 0)
+            {
+                return default(T);
+            }
+            
+            var item = stack.Pop();
+
+            if (stack.Count == 0)
+            {
+                _currentStackIndex--;
+                _listOfStacks.RemoveAt(_listOfStacks.Count - 1);
+            }
+
+            return item;
         }
 
-        public T PopAt()
-        {            
+        public T PopAt(int index)
+        {
+            if (index == _currentStackIndex)
+            {
+                return Pop();
+            }
+
+            var stack = _listOfStacks[index];
+            var item = stack.Pop();
+
+            if (stack.Count == 0)
+            {
+                _currentStackIndex--;
+                _listOfStacks.Remove(stack);
+            }
+
+            return item;
         }
     }
 }
--- CrackingCodingInterview/CrackingCodingInterview/StacksAndQueues/Task33.cs
+++ CrackingCodingInterview/CrackingCodingInterview/StacksAndQueues/Task33.cs
@@ -1,10 +1,25 @@
+using System;
+
 namespace CrackingCodingInterview.StacksAndQueues
 {
     public class Task33
     {
         public Task33()
         {
+            var stacks = new SetOfStacks<int>(1);
             
+                stacks.Push(1);
+                stacks.Push(2);
+                stacks.Push(3);
+
+            Console.WriteLine("data: " + stacks.PopAt(0));
+            Console.WriteLine("stack index: " + stacks.CurrentStackIndex);
+            Console.WriteLine("data: " + stacks.PopAt(0));
+            Console.WriteLine("stack index: " + stacks.CurrentStackIndex);
+            Console.WriteLine("data: " + stacks.PopAt(0));
+            Console.WriteLine("stack index: " + stacks.CurrentStackIndex);
+            Console.WriteLine("data: " + stacks.PopAt(0));
+            Console.WriteLine("stack index: " + stacks.CurrentStackIndex);
         }
     }
 }
